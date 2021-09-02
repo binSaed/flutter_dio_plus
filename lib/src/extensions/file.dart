c7c2349e4c24eff1353c5b2463de2e9195abf7ba
Abdelrahman Saed <me@abdosaed.ml>
2021-09-02 15:54:55 +0200

    add new extension

--- lib/src/extensions/file.dart
+++ lib/src/extensions/file.dart
@@ -20,11 +20,23 @@ extension FileX on File {
 
 extension FilesX on List<File> {
   Future<List<MultipartFile>> toMultipart() async {
+    //[File, File]=>[MultipartFile, MultipartFile]
     final List<MultipartFile> multipartFiles = <MultipartFile>[];
     for (final File file in (this ?? <File>[])) {
       multipartFiles.add(await file.toMultipartFile());
     }
 
     return multipartFiles;
+  }
+
+  Future<Map<String, MultipartFile>> toMultipartMap(String key) async {
+    //[File, File]=>{key[0]:MultipartFile, key[1]:MultipartFile}
+    final multipartMap = <String, MultipartFile>{};
+    final files = await toMultipart();
+    final length = files.length;
+    for (var i = 0; i < length; i++) {
+      multipartMap['$key[$i]'] = files[i];
+    }
+    return multipartMap;
   }
 }
